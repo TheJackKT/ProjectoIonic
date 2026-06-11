21a16fb2ba8c10e474ed00681b311aab1d375739
jazmin <jazminpaolachaconmora18@gmail.com>
2026-06-10 20:24:24 -0500

    Se agregan archivos añadidos por complicaciones en git

---
+++ .vite/deps/p-D7bSXQD3-AjvIYanW.js
@@ -0,0 +1,26 @@
+import { Sn as m, yn as c } from "./dist-LgcXgdsJ.js";
+import { r as H, s as P } from "./p-CHqRYvYm-CXtH-2sc.js";
+import { o as e } from "./p-Baq1XyAy-BkFWhHan.js";
+//#region node_modules/@ionic/core/components/p-D7bSXQD3.js
+/*!
+* (C) Ionic http://ionicframework.com - MIT License
+*/
+var i = () => {
+	const i = window;
+	i.addEventListener("statusTap", (() => {
+		H((() => {
+			const o = document.elementFromPoint(i.innerWidth / 2, i.innerHeight / 2);
+			if (!o) return;
+			const n = m(o);
+			n && new Promise(((o) => e(n, o))).then((() => {
+				P((async () => {
+					n.style.setProperty("--overflow", "hidden"), await c(n, 300), n.style.removeProperty("--overflow");
+				}));
+			}));
+		}));
+	}));
+};
+//#endregion
+export { i as startStatusTap };
+
+//# sourceMappingURL=p-D7bSXQD3-AjvIYanW.js.map
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,6 +20,7 @@ const usuarios = [
       usuario: 'Javier',
       clave: 'waos'
     },
+
     {
       usuario: 'Jazmin',
       clave: '182'
@@ -62,7 +63,7 @@ const Login: React.FC = () => {
 
       <IonCardContent className="card-login"> 
 
-     <IonTitle>Agenda de Contactos {user}</IonTitle>
+     <IonTitle>Agenda de Contactos</IonTitle>
         <IonItem>
        
           <IonInput
